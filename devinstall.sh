30f4e894eae18d96178b630e5b7c5ab7b4b7b50d
Alex <sahok6264@gmail.com>
2021-11-18 22:15:32 +0200

    Remove persist store

--- devinstall.sh
+++ devinstall.sh
@@ -2,24 +2,24 @@
 cd ~
 pwd
 
-rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/time-planning-pn
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/greate-belt-pn
 
-cp -a Documents/workspace/microting/eform-time-planning-plugin/eform-client/src/app/plugins/modules/time-planning-pn Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/time-planning-pn
+cp -a Documents/workspace/microting/eform-greate-belt-plugin/eform-client/src/app/plugins/modules/greate-belt-pn Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/greate-belt-pn
 
 mkdir -p Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins
 
-rm -fR Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/TimePlanning.Pn
+rm -fR Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/GreateBelt.Pn
 
-cp -a Documents/workspace/microting/eform-time-planning-plugin/eFormAPI/Plugins/TimePlanning.Pn Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/TimePlanning.Pn
+cp -a Documents/workspace/microting/eform-greate-belt-plugin/eFormAPI/Plugins/GreateBelt.Pn Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/GreateBelt.Pn
 
 # Test files rm
-rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-settings
-rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-general
-rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/TimePlanning
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/greate-belt-settings
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/greate-belt-general
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/GreateBelt
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
 
 # Test files cp
-cp -a Documents/workspace/microting/eform-time-planning-plugin/eform-client/e2e/Tests/time-planning-settings Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-settings
-cp -a Documents/workspace/microting/eform-time-planning-plugin/eform-client/e2e/Tests/time-planning-general Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-general
-cp -a Documents/workspace/microting/eform-time-planning-plugin/eform-client/e2e/Page\ objects/TimePlanning Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/TimePlanning
-cp -a Documents/workspace/microting/eform-time-planning-plugin/eform-client/wdio-headless-plugin-step2.conf.js Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
+cp -a Documents/workspace/microting/eform-greate-belt-plugin/eform-client/e2e/Tests/greate-belt-settings Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/greate-belt-settings
+cp -a Documents/workspace/microting/eform-greate-belt-plugin/eform-client/e2e/Tests/greate-belt-general Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/greate-belt-general
+cp -a Documents/workspace/microting/eform-greate-belt-plugin/eform-client/e2e/Page\ objects/GreateBelt Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/GreateBelt
+cp -a Documents/workspace/microting/eform-greate-belt-plugin/eform-client/wdio-headless-plugin-step2.conf.js Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
