27c4f61f9db6fcc58fc0f064ad4edb323405b059
René Schultz Madsen <rm@microting.com>
2023-04-10 06:00:56 +0000

    Fixing angular14 build steps

--- devinstall.sh
+++ devinstall.sh
@@ -16,10 +16,10 @@ cp -a Documents/workspace/microting/eform-angular-greate-belt-plugin/eFormAPI/Pl
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/greate-belt-settings
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/greate-belt-general
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/GreateBelt
-rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.ts 
 
 # Test files cp
 cp -a Documents/workspace/microting/eform-angular-greate-belt-plugin/eform-client/e2e/Tests/greate-belt-settings Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/greate-belt-settings
 cp -a Documents/workspace/microting/eform-angular-greate-belt-plugin/eform-client/e2e/Tests/greate-belt-general Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/greate-belt-general
 cp -a Documents/workspace/microting/eform-angular-greate-belt-plugin/eform-client/e2e/Page\ objects/GreateBelt Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/GreateBelt
-cp -a Documents/workspace/microting/eform-angular-greate-belt-plugin/eform-client/wdio-headless-plugin-step2.conf.js Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
+cp -a Documents/workspace/microting/eform-angular-greate-belt-plugin/eform-client/wdio-headless-plugin-step2.conf.ts  Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.ts 
